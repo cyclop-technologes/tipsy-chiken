26c06d930e6ae9c3529ad1a3533939e660d5beaf
Dmitriy Kovalev <d.g.kovalev@gmail.com>
2019-05-08 11:13:23 +0300

    step 1+2

--- src/components/HowToPlay.vue
+++ src/components/HowToPlay.vue
@@ -1,11 +1,34 @@
 <template>
   <section class="bg-container">
     <div class="container d-flex align-items-center flex-column">
-      <h1 class="htp font-weight-bold">HOW TO PLAY</h1>
-      <h1 class="fss font-weight-bold">5 SIMPLE STEPS</h1>
+      <h1 class="how-to-play font-weight-bold">HOW TO PLAY</h1>
+      <h1 class="five-simple-steps font-weight-bold">5 SIMPLE STEPS</h1>
     </div>
-    <div class="container first-step position-relative">
-      <img src="../assets/img/team.svg">
+    <div class="container first-step position-relative d-flex justify-content-between  mt-1">
+      <div class="step font-weight-bold">1</div>
+      <img class="center-absolute" src="../assets/img/team.svg">
+      <div class="info font-weight-bold text-right">
+        <div class="info-top text-uppercase">
+          <p>Always keep your <br>
+            shot glass full <br>
+            and a cocktail<br>
+            chaser handy.</p>
+        </div>
+        <div class="info-bottom">
+          <p>Gather 3-9 players.</p>
+          <p>Play for 30-90 minutes.</p>
+        </div>
+      </div>
+    </div>
+    <div class="container second-step ">
+      <div class="rooster-box position-relative d-flex justify-content-between">
+        <img class="center-absolute" src="../assets/img/rooster.svg">
+        <div class="step font-weight-bold">2</div>
+        <div class="info font-weight-bold text-right text-uppercase">
+          <p>draw a Dare card.</p>
+        </div>
+      </div>
+      <div class="cards text-center"><img src="../assets/img/cards.svg"></div>
     </div>
   </section>
 </template>
@@ -18,18 +41,43 @@
   background: url('../assets/img/bg.svg') 0 0 repeat;
 }
 
-.htp {
+.how-to-play {
   color: $primary;
 }
 
-.fss {
+.five-simple-steps {
   color: $secondary;
 }
 
+.center-absolute {
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 500px;
+}
+
 .first-step {
-  img {
-    position: absolute;
-  }
+	padding-bottom: 200px;
+}
+
+.rooster-box {
+	padding-bottom: 200px;
+}
+
+.step {
+  color: $primary;
+  font-size: 100px;
+}
+
+.info {
+  color: $white;
+  font-size: 30px;
+  line-height: 36px;
+}
+
+.info-bottom {
+  padding-top: 200px;
 }
 
 </style>
