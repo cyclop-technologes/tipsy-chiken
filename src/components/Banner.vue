75e0c30826aeed3344af71149f0bfeba89e5dc05
Ruslan Vasiliev <ruzero.io@gmail.com>
2019-05-14 18:33:37 +0300

    responsive banner

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,24 +1,26 @@
 <template lang="html">
-  <section id="banner" class="banner vh-100 min-vh-100 d-flex flex-column">
-    <div class="container flex-grow-1">
-      <div class="row h-100">
+  <section id="banner" class="banner min-vh-100">
+    <div class="container min-vh-100">
+      <div class="row min-vh-100">
         <div data-aos="zoom-out-right"
           class="col-sm-12 col-lg-6 position-relative align-self-center">
           <img class="img-main" src="../assets/img/box.png" alt="">
         </div>
-        <div class="col-sm-12
-        col-lg-6
-        align-self-center
-        text-center
-        h-100
-        d-flex
-        flex-column
-        justify-content-between">
+        <div class="
+          main-container
+          col-sm-12
+          col-lg-6
+          align-self-center
+          text-center
+          h-100
+          d-flex
+          flex-column
+          justify-content-between">
           <div data-aos="zoom-out-left" class="top-block">
-            <div class="mt-5 pt-5">
+            <div class="banner-logo mt-5 pt-5">
               <img src="../assets/img/logo-inline.svg" alt="">
             </div>
-            <div class="mt-5">
+            <div class="mt-sm-0 mt-md-5">
               <b-button class="px-5 py-4" variant="warning">BUY ON AMAZON</b-button>
             </div>
           </div>
@@ -83,7 +85,7 @@
         z-index: 0;
         position: absolute;
         left: 0;
-        top: -35px;
+        top: -1rem;
         width: 100%;
     }
 }
@@ -94,6 +96,15 @@
     transform: translateY(0);
     width: 100%;
     margin-top: 6rem;
+  }
+  .banner-logo {
+    display: none;
+  }
+  .main-container{
+    height: auto;
+  }
+  .top-block{
+    margin-top: 1rem;
   }
 }
 </style>
