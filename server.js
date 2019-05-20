605f8ab7bb5e9ae895205bb3abaf2b1b79db4519
Ruslan Vasiliev <ruzero.io@gmail.com>
2019-05-20 23:02:03 +0300

    async

--- server.js
+++ server.js
@@ -2,25 +2,36 @@ const express = require("express");
 const port = process.env.PORT || 8080;
 const app = express();
 const bodyParser = require('body-parser');
+const async = require('async');
 const GoogleSpreadsheet = require('google-spreadsheet');
 const doc = new GoogleSpreadsheet('1DEFkYE5vDo-NrHLMnt1a9A9fGV1MkGFy86humWb0gAc');
 let sheet;
+let creds_json
 // const creds = require('./client_secret.json')
 
 app.use(bodyParser.urlencoded({ extended: false }))
-let creds_json = {
-  client_email: process.env.client_email,
-  private_key: process.env.private_key,
-};
-console.log(creds_json);
-doc.useServiceAccountAuth(creds_json, function() {
-  doc.getInfo(function(err, info) {
-    sheet = info.worksheets[0];
-    sheet.setHeaderRow(['cardTitle', 'animal', 'subject', 'points', 'name', 'email']);
 
+async.series([
+  (step) => {
+    creds_json = {
+      client_email: process.env.client_email,
+      private_key: process.env.private_key,
+    };
+
+    step()
+  },
+  (step) => {
+    console.log(creds_json);
+    doc.useServiceAccountAuth(creds_json, function() {
+      doc.getInfo(function(err, info) {
+        sheet = info.worksheets[0];
+        sheet.setHeaderRow(['cardTitle', 'animal', 'subject', 'points', 'name', 'email']);
+
+      });
+    });
     console.log(sheet)
-  });
-});
+  }
+])
 
 
 app.use(express.static(__dirname + "/dist/"));
