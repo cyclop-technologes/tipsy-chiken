c318faa9d17683bcc179829562d8e1789a859662
Ruslan Vasiliev <ruzero.io@gmail.com>
2019-05-23 13:27:45 +0300

    accordion

--- src/components/Faq.vue
+++ src/components/Faq.vue
@@ -1,74 +1,83 @@
 <template>
-  <section class="pt-5">
-    <div class="faq-header text-center">
-      <h1 class="text-uppercase font-weight-bold">
-        <span class="color-primary">Frequently Asked</span><br>
-        <span class="color-secondary">Questions</span>
-      </h1>
-    </div>
-    <div class="container faq-container">
-      <div role="tablist">
-        <b-card no-body class="mb-1">
-      <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-button block href="#" v-b-toggle.accordion-1 variant="info">Accordion 1</b-button>
-      </b-card-header>
-      <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
-        <b-card-body>
-          <b-card-text>I start opened because <code>visible</code> is <code>true</code></b-card-text>
-          <b-card-text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</b-card-text>
-        </b-card-body>
-      </b-collapse>
-    </b-card>
-
-    <b-card no-body class="mb-1">
-      <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-button block href="#" v-b-toggle.accordion-2 variant="info">Accordion 2</b-button>
-      </b-card-header>
-      <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
-        <b-card-body>
-          <b-card-text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</b-card-text>
-        </b-card-body>
-      </b-collapse>
-    </b-card>
-
-    <b-card no-body class="mb-1">
-      <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-button block href="#" v-b-toggle.accordion-3 variant="info">Accordion 3</b-button>
-      </b-card-header>
-      <b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
-        <b-card-body>
-          <b-card-text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</b-card-text>
-        </b-card-body>
-      </b-collapse>
-    </b-card>
+<section class="pt-5">
+  <div class="faq-header text-center">
+    <h1 class="text-uppercase font-weight-bold">
+      <span class="color-primary">Frequently Asked</span><br>
+      <span class="color-secondary">Questions</span>
+    </h1>
   </div>
-
-
-
-
-
+  <div class="container faq-container">
+    <div class="row">
+      <div class="col-12 col-lg-6">
+        <div role="tablist">
+          <b-card v-for="(item, index) in leftColumn" :key="index" no-body class="mb-1">
+            <b-card-header header-tag="header" class="p-1" role="tab">
+              <b-button block href="#" v-b-toggle="'accordion-left-' + index" variant="info">{{item.title}}</b-button>
+            </b-card-header>
+            <b-collapse :id="`accordion-left-${index}`" role="tabpanel">
+              <b-card-body>
+                <b-card-text>{{item.content}}</b-card-text>
+              </b-card-body>
+            </b-collapse>
+          </b-card>
+        </div>
+      </div>
+      <div class="col-12 col-lg-6">
+        <div role="tablist">
+          <b-card v-for="(item, index) in items" :key="index" no-body class="mb-1">
+            <b-card-header header-tag="header" class="p-1" role="tab">
+              <b-button block href="#" v-b-toggle="'accordion-right-' + index" variant="info">{{item.title}}</b-button>
+            </b-card-header>
+            <b-collapse :id="`accordion-right-${index}`" role="tabpanel">
+              <b-card-body>
+                <b-card-text>{{item.content}}</b-card-text>
+              </b-card-body>
+            </b-collapse>
+          </b-card>
+        </div>
       </div>
     </div>
-  </section>
+  </div>
+</section>
 </template>
 <script>
+const faq = require('../faq/faq.json').faq
+export default {
+  data() {
+    return {
+      items: faq,
+    }
+  },
+  computed: {
+    halfLength() {
+      const vm = this;
+      return Math.ceil(vm.items.length / 2);
+    },
+    leftColumn() {
+      const vm = this;
+      return this.items.splice(0, vm.halfLength);
+    },
+    // rightColumn() {
+    //   const vm = this;
+    //   return this.items.splice(vm.halfLength, vm.items.length - 1 );
+    // }
+  }
+}
 </script>
 <style lang="scss">
 .faq-header {
-  margin-top: 12.5rem;
+    margin-top: 12.5rem;
 }
-  .color-primary {
+.color-primary {
     color: $primary;
-  }
-  .color-secondary {
+}
+.color-secondary {
     color: $secondary;
-  }
-  .cards-column {
+}
+.cards-columns {
     column-count: 2;
     @include media-breakpoint-down(md) {
-      column-count: 1;
+        column-count: 1;
     }
-  }
-
-
+}
 </style>
