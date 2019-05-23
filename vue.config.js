3a3d8efe5e6e886e7cc52ec9cbce7a165c3fd8f0
Ruslan Vasiliev <ruzero.io@gmail.com>
2019-05-23 10:36:26 +0300

    fix vue config

--- vue.config.js
+++ vue.config.js
@@ -8,44 +8,24 @@ module.exports = {
   },
   pages: {
     'index': {
-      // entry for the page
-      entry: 'src/index/main.js',
-      // the source template
-      template: 'public/index.html',
-      // output as dist/index.html
+      entry: 'src/index/main.js',  // the source template
+      template: 'public/index.html',// output as dist/index.html
       filename: 'index.html',
-      // when using title option,
-      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
       title: 'Index Page',
-      // chunks to include on this page, by default includes
-      // extracted common chunks and vendor chunks.
       chunks: ['chunk-vendors', 'chunk-common', 'index']
     },
     'newcard': {
-      entry: 'src/newcard/main.js',
-      // the source template
-      template: 'public/index.html',
-      // output as dist/index.html
+      entry: 'src/newcard/main.js',  // the source template
+      template: 'public/index.html',// output as dist/index.html
       filename: 'newcard.html',
-      // when using title option,
-      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
       title: 'Newcard',
-      // chunks to include on this page, by default includes
-      // extracted common chunks and vendor chunks.
       chunks: ['chunk-vendors', 'chunk-common', 'newcard']
     },
     'faq': {
-      // entry for the page
-      entry: 'src/index/main.js',
-      // the source template
-      template: 'public/index.html',
-      // output as dist/index.html
+      entry: 'src/faq/main.js',  // the source template
+      template: 'public/index.html',// output as dist/index.html
       filename: 'faq.html',
-      // when using title option,
-      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
       title: 'Index Page',
-      // chunks to include on this page, by default includes
-      // extracted common chunks and vendor chunks.
       chunks: ['chunk-vendors', 'chunk-common', 'faq']
     },
   }
