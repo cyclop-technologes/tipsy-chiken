a3a4d88c38149a6e360e4e08fb05cc7ed7c02b42
Dmitriy Kovalev <d.g.kovalev@gmail.com>
2019-06-05 12:56:29 +0300

    subscribe form legal information

--- src/components/Subscribe.vue
+++ src/components/Subscribe.vue
@@ -3,6 +3,7 @@
     <h1 class="text-uppercase font-weight-bold mb-3">
       <span class="text-primary">Subscribe to our </span><span class="text-secondary"> newsletter</span>
     </h1>
+    <div><p class="text-white">If you’d like us to email you when we release something new, enter your address here.</p></div>
     <form class="subscribe-form mx-auto">
       <v-text-field
       v-model="name"
@@ -24,7 +25,10 @@
       @input="$v.email.$touch()"
       @blur="$v.email.$touch()"
     ></v-text-field>
-
+    <p class="text-white">By submitting this form, you are consenting to receive emails from:
+      Tipsy Chicken, 5620 1st Ave Suite 4, Brooklyn , NY, 11220, US.
+      You can revoke your consent to receive emails at any time by using the Unsubscribe button,
+      found at the bottom of every email.</p>
     <b-button class="px-5 py-3 mt-3 mb-5 text-shadow" variant="warning">SUBSCRIBE</b-button>
     </form>
   </div>
