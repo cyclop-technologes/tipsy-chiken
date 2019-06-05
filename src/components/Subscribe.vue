59d05e261cb078145e7f10adf606a29a12163c67
Ruslan Vasiliev <ruzero.io@gmail.com>
2019-06-05 16:34:53 +0300

    add submission

--- src/components/Subscribe.vue
+++ src/components/Subscribe.vue
@@ -4,32 +4,34 @@
       <span class="text-primary">Subscribe to our </span><span class="text-secondary"> newsletter</span>
     </h1>
     <div><p class="text-white">If you’d like us to email you when we release something new, enter your address here.</p></div>
-    <form class="subscribe-form mx-auto">
+    <form action="https://tipsychickengame.us20.list-manage.com/subscribe/post?u=82f9a822bcdf0c2ce9c5846d1&amp;id=6990e00fdc" method="post" target="_blank" class="subscribe-form mx-auto">
       <v-text-field
-      v-model="name"
-      :error-messages="nameErrors"
-      label="Name"
-      color='#FCCD3F'
-      required
-      dark
-      @input="$v.name.$touch()"
-      @blur="$v.name.$touch()"
-    ></v-text-field>
-    <v-text-field
-      v-model="email"
-      :error-messages="emailErrors"
-      label="E-mail"
-      color='#FCCD3F'
-      required
-      dark
-      @input="$v.email.$touch()"
-      @blur="$v.email.$touch()"
-    ></v-text-field>
-    <p class="text-white form-legacy">By submitting this form, you are consenting to receive emails from:
-      Tipsy Chicken, 5620 1st Ave Suite 4, Brooklyn , NY, 11220, US.
-      You can revoke your consent to receive emails at any time by using the Unsubscribe button,
-      found at the bottom of every email.</p>
-    <b-button class="px-5 py-3 mt-3 mb-5 text-shadow" variant="warning">SUBSCRIBE</b-button>
+        v-model="name"
+        :error-messages="nameErrors"
+        label="Name"
+        color='#FCCD3F'
+        name='FNAME'
+        required
+        dark
+        @input="$v.name.$touch()"
+        @blur="$v.name.$touch()"
+      ></v-text-field>
+      <v-text-field
+        v-model="email"
+        :error-messages="emailErrors"
+        label="E-mail"
+        color='#FCCD3F'
+        name='EMAIL'
+        required
+        dark
+        @input="$v.email.$touch()"
+        @blur="$v.email.$touch()"
+      ></v-text-field>
+      <p class="text-white form-legacy">By submitting this form, you are consenting to receive emails from:
+        Tipsy Chicken, 5620 1st Ave Suite 4, Brooklyn , NY, 11220, US.
+        You can revoke your consent to receive emails at any time by using the Unsubscribe button,
+        found at the bottom of every email.</p>
+      <b-button type="submit" class="px-5 py-3 mt-3 mb-5 text-shadow" variant="warning">SUBSCRIBE</b-button>
     </form>
   </div>
 </template>
@@ -41,7 +43,7 @@ export default {
       name: '',
       email: '',
     }
-  },
+  }
 }
 </script>
 
