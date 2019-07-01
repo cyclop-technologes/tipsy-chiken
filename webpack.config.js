fc872b1bb5d871eafa7896cdecebc4cb62ad2919
Ruslan Vasiliev <ruzero.io@gmail.com>
2019-07-01 15:06:25 +0300

    fix form

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,23 @@
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
 module.exports = {
+  configureWebpack: () => {
+    if (process.env.NODE_ENV !== 'production') return;
+    return {
+      plugins: [
+        new PrerenderSPAPlugin(
+          // Absolute path to compiled SPA
+          path.resolve(__dirname, 'dist'),
+          // List of routes to prerender
+          [ '/'],
+          {
+            // options
+          }
+        ),
+      ]
+    }
+  },
   module: {
     loaders: [
       {
